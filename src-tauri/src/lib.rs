5972e96ed4c8b778841a6eb12d0421b6b2b9fddb
Charles Schaefer <charlesschaefer@gmail.com>
2024-07-08 19:22:30 -0300

    Removing a command handler for rust -> js from lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -129,7 +129,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_notification::init())
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
