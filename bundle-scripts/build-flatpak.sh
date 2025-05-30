5811af2a36e0b90ccd339f575053e48f9b3b2c0d
Charles Schaefer <charlesschaefer@gmail.com>
2025-05-30 04:19:27 -0300

    Trying to build the package from sources instead that using upstream deb

--- bundle-scripts/build-flatpak.sh
+++ bundle-scripts/build-flatpak.sh
@@ -2,6 +2,8 @@
 CURL=$(which curl)
 FLATPAK_BUILDER=$(which flatpak-builder)
 GIT=$(which git)
+PYTHON=$(which python3)
+PIP=$(which pip3)
 
 error() {
     echo -e "\033[31m$1\033[0m" # Print the message in red
@@ -20,6 +22,16 @@ success() {
     echo -e "\033[32m$1\033[0m" # Print the message in green
 }
 
+dbg() {
+    first_word=$(echo "$1" | cut -d"_" -f1)
+    if [ $(echo "$1" | cut -c1) != "[" ] && [ "$first_word" != "error" ] && [ "$first_word" != "info" ] && [ "$first_word" != "success" ]; then
+        echo -e "\033[90mRunning: $1\033[0m" # Print the message in gray
+    fi
+}
+
+# Generates debug messages for each command executed
+trap 'dbg "$BASH_COMMAND"; this_command=$BASH_COMMAND' DEBUG
+
 if [ -z "$CURL" ]; then
     error_and_exit "curl is not installed. Please install curl to proceed."
     exit 1
@@ -32,6 +44,77 @@ if [ -z "$GIT" ]; then
     error_and_exit "git is not installed. Please install git to proceed."
     exit 1
 fi
+if [ -z "$PYTHON" ]; then
+    error_and_exit "python3 is not installed. Please install python3 to proceed."
+    exit 1
+fi
+if [ -z "$PIP" ]; then
+    error_and_exit "pip3 is not installed. Please install pip3 to proceed."
+    exit 1
+fi
+
+
+info "Downloading flatpak-cargo-generator.py script from flatpak-builder-tools repository..."
+$CURL --location -o flatpak-cargo-generator.py "https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/refs/heads/master/cargo/flatpak-cargo-generator.py"
+if [ $? -ne 0 ]; then
+    error_and_exit "Failed to download flatpak-cargo-generator.py."
+    exit 1
+else 
+    success "flatpak-cargo-generator.py downloaded successfully."
+fi
+
+
+info "Installing flatpak_node_generator scripts from flatpak-builder-tools repository..."
+$PIP install "git+https://github.com/flatpak/flatpak-builder-tools.git#subdirectory=node"
+if [ $? -ne 0 ]; then
+    error_and_exit "Failed to install flatpak_node_generator."
+    exit 1
+else 
+    success "flatpak_node_generator installed successfully."
+fi
+
+# info "Downloading flatpak-json2yaml.py script from flatpak-builder-tools repository..."
+# $CURL --location -o flatpak-json2yaml.py "https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/refs/heads/master/flatpak-json2yaml.py"
+# if [ $? -ne 0 ]; then
+#     error_and_exit "Failed to download flatpak-json2yaml.py."
+#     exit 1
+# else 
+#     success "flatpak-json2yaml.py downloaded successfully."
+# fi
+
+info "Generating cargo-sources.json from Cargo.lock..."
+$PYTHON ./flatpak-cargo-generator.py ../src-tauri/Cargo.lock -o cargo-sources.json 
+if [ $? -ne 0 ]; then
+    error_and_exit "Failed to generate cargo-sources.json."
+    exit 1
+else
+    success "cargo-sources.json generated successfully."
+fi
+
+info "Generating node-sources.json from package-lock.json..."
+$PYTHON -m flatpak_node_generator --output="npm-sources.json" -s npm ../package-lock.json
+if [ $? -ne 0 ]; then
+    error_and_exit "Failed to generate node-sources.json."
+    exit 1
+else
+    success "node-sources.json generated successfully."
+fi
+
+info "Installing flathub shared-modules from github"
+rm -Rf shared-modules && $GIT clone https://github.com/flathub/shared-modules.git 
+if [ $? -ne 0 ]; then
+    error_and_exit "Failed to clone the flathub shared-modules repository."
+    exit 1
+fi
+
+# info "Generating libayatana.yaml from ..."
+# $PYTHON flatpak-json2yaml.py ./libayatana-appindicator/libayatana-appindicator-gtk3.json --output="./libayatana-appindicator/libayatana-appindicator-gtk3.yaml" -f
+# if [ $? -ne 0 ]; then
+#     error_and_exit "Failed to generate node-sources.json."
+#     exit 1
+# else
+#     success "node-sources.json generated successfully."
+# fi
 
 # $CURL --location -o latest "https://github.com/charlesschaefer/addiction-tracker/releases/latest"
 # if [ $? -ne 0 ]; then
@@ -47,15 +130,11 @@ fi
 #     exit 1
 # fi
 
-info "Installing flathub shared-modules from github"
-$GIT submodule add --force https://github.com/flathub/shared-modules.git 
-if [ $? -ne 0 ]; then
-    error_and_exit "Failed to add the flathub shared-modules submodule."
-    exit 1
-fi
+
+
 
 info "Calling flatpak-builder to build the flatpak"
-$FLATPAK_BUILDER --arch="x86_64" --delete-build-dirs --force-clean --sandbox --user --install --install-deps-from=flathub --ccache --repo=local_repo build-dir net.charlesschaefer.addictiontracker.yml
+$FLATPAK_BUILDER --arch="x86_64" --delete-build-dirs --force-clean --sandbox --user --install --install-deps-from=flathub --mirror-screenshots-url=https://dl.flathub.org/media/ --ccache --repo=local_repo build-dir net.charlesschaefer.addictiontracker.yml
 if [ $? -ne 0 ]; then
     error "Failed to build the flatpak!"
     info "Fix the errors described above and try again by running the following command:"
