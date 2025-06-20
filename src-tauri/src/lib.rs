e65503e5c6e8c47109c7003a544b0458112dae13
Charles Schaefer <charlesschaefer@gmail.com>
2025-06-20 13:54:04 -0300

    Adding formatted license file to the license component; Removing the tray icon declaration from tauri.conf.json

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -32,44 +32,17 @@ pub fn run() {
             desktop::set_frontend_complete,
             #[cfg(not(desktop))]
             android::save_backup_file,
-            //add_notification,
-            // get_google_secrets
         ]
     );
 
-    // let v1 = env!("GOOGLE_GEMINI_PROJECT_NAME").to_string();
-    // let v2 = env!("GOOGLE_GEMINI_API").to_string();
-    // println!("Valores da API: {} => {}", v1, v2);
-    
     builder
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
 
-// #[tauri::command]
-// fn add_notification(app_handle: tauri::AppHandle, title: String, body: String) {
-//     use tauri_plugin_notification::NotificationExt;
-//     let _ = app_handle.emit("msg", "Starting to build the notification");
-//     let mut builder = app_handle.notification().builder();
-//     let _ = app_handle.emit("msg", "Notification built. Let's add title and body");
-//     builder = builder.title(title)
-//         .body(body);
-//     let _ = app_handle.emit("msg", "Notification ready. Let's show it");
-//     builder.show()
-//         .unwrap();
-// }
-
 #[derive(Deserialize, Serialize)]
 struct Secrets {
     project: String,
     api_key: String,
 }
-
-// #[tauri::command]
-// fn get_google_secrets() -> Secrets {
-//     return Secrets {
-//         project: env!("GOOGLE_GEMINI_PROJECT_NAME").to_string(),
-//         api_key: env!("GOOGLE_GEMINI_API").to_string(),
-//     }
-// }
